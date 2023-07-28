6be1c8037854ca207d635b8c483b22f549a72d10
Lihour <lihou.kong@lyhourpay.com>
2023-07-28 11:54:05 +0700

    first commit

---
+++ MenuPager/MenuPager.swift
@@ -0,0 +1,15 @@
+//
+//  MenuPager.swift
+//  MenuPager
+//
+//  Created by Lihour on 28/7/23.
+//
+
+import UIKit
+
+
+class MenuPager: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
