14d0637eb5d3670b85360e46f420be562f3d6826
Lihour <lihou.kong@lyhourpay.com>
2023-07-28 11:45:25 +0700

    Initalize project setup

---
+++ MenuPager/MenuPager.h
@@ -0,0 +1,18 @@
+//
+//  MenuPager.h
+//  MenuPager
+//
+//  Created by Lihour on 28/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MenuPager.
+FOUNDATION_EXPORT double MenuPagerVersionNumber;
+
+//! Project version string for MenuPager.
+FOUNDATION_EXPORT const unsigned char MenuPagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MenuPager/PublicHeader.h>
+
+
---
+++ MenuPagerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuPagerExample
+//
+//  Created by Lihour on 28/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
